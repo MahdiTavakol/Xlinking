6bf54527a86006211c92ca56c460ba2db782794a
MahdiTavakol <61837861+MahdiTavakol@users.noreply.github.com>
2024-08-11 13:11:53 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
 /*
 * A complicated program to add AGE into the Coarse grained collagen
 * CG files.
-* i do not guarantee that it works for your purpose!!!
+* I do not guarantee that it works for your purpose!!!
 * mahditavakol90@gmail.com
 */
 
